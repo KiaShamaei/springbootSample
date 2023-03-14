63e36f97bec9a2d8134ebe9c253e8a681ee5be16
kiarash.shamaii@gmail.com <kiarash.shamaii@gmail.com>
2023-03-14 08:10:22 +0330

    add service and endpoint for download and upload

--- src/main/java/com/example/springbootsample/SpringBootSampleApplication.java
+++ src/main/java/com/example/springbootsample/SpringBootSampleApplication.java
@@ -3,6 +3,8 @@ package com.example.springbootsample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,6 +12,10 @@ public class SpringBootSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSampleApplication.class, args);
+    }
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
